4822516d88dd283c42c0d6b4d7ee011c1425c59f
Aaruran Elamurugaiyan <aarurane@gmail.com>
2019-08-02 02:18:15 -0400

    fixed allowed domains

--- main.go
+++ main.go
@@ -9,18 +9,32 @@ import (
 func main() {
 	// Instantiate default collector
 	c := colly.NewCollector(
-		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
-		colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
+		colly.AllowedDomains("docs.python.org", "http://docs.python.org",
+		"https://docs.python.org", "https://docs.python.org/3/"),
 	)
 
 	// On every a element which has href attribute call callback
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
+		absoluteLink := e.Request.AbsoluteURL(link)
 		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		fmt.Printf("Link found: %q -> %s\n", e.Text, absoluteLink)
 		// Visit link found on page
 		// Only those links are visited which are in AllowedDomains
-		c.Visit(e.Request.AbsoluteURL(link))
+		// fmt.Printf("Body: %s\n", e.ChildText("body"))
+		c.Visit(absoluteLink)
+	})
+
+	// On every a element which has span attribute call callback
+	c.OnHTML("#section", func(e *colly.HTMLElement) {
+		fmt.Println(e.ChildText("p"))
+		// class := e.Attr("class")
+		// // Print link
+		// fmt.Printf("Class: %s\n", class)
+		// // Visit link found on page
+		// // Only those links are visited which are in AllowedDomains
+		// // fmt.Printf("Body: %s\n", e.ChildText("body"))
+		// // c.Visit(absoluteLink)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -29,6 +43,7 @@ func main() {
 	})
 
 	// Start scraping on seed
-  seed := "https://hackerspaces.org"
+  // seed := "https://godoc.org/"
+  seed := "https://docs.python.org/3/tutorial/index.html"
 	c.Visit(seed)
 }
