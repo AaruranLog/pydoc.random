88a866f70fbbb9e57a4296a539833e8ea51840c6
Aaruran Elamurugaiyan <aarurane@gmail.com>
2019-08-03 00:11:27 -0400

    a minimal, working scraper of the python docs

--- main.go
+++ main.go
@@ -3,8 +3,31 @@ package main
 import (
 	"fmt"
 	"time"
+	"os"
+	"strings"
 	"github.com/gocolly/colly"
 )
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func writeDocToFile(filename, body string) {
+	if _, err := os.Stat("docs/" + filename); err == nil {
+		fmt.Println("File already written.")
+		return
+	}
+	f, err := os.Create("docs/" + filename)
+	check(err)
+	defer f.Close()
+	cleanedBody := strings.TrimSpace(body)
+	cleanedBody = strings.Replace(cleanedBody, "¶", "\n")
+	_, err = f.WriteString(cleanedBody)
+	f.Sync()
+	fmt.Println("Written to file:", filename)
+}
 
 func main() {
 	// Instantiate default collector
@@ -13,30 +36,38 @@ func main() {
 		"https://docs.python.org", "https://docs.python.org/3/"),
 	)
 
-	// On every a element which has href attribute call callback
+	c.OnResponse(func(r * colly.Response){
+		fmt.Println("Response received from:", r.Request.URL)
+	})
+
+	// Basic error-handling
+	c.OnError(func(_ *colly.Response, err error){
+		fmt.Println("Something went wrong during request:", err)
+		check(err)
+	})
+
+	c.OnHTML("div[class=document]", func(e *colly.HTMLElement) {
+		documentBody := e.Text
+
+		absoluteURL := e.Request.URL.String()
+		descriptiveName := strings.Split(absoluteURL, "/3/")[1]
+		filename := strings.Replace(descriptiveName, "/", "-", -1)
+		filename = strings.Split(filename, ".")[0] + ".txt"
+		writeDocToFile(filename, documentBody)
+	})
+
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 		absoluteLink := e.Request.AbsoluteURL(link)
-		// Print link
+
 		if e.Text == "next" {
 			c.Visit(absoluteLink)
 		}
 	})
 
-	// On every a element which has span attribute call callback
-	c.OnHTML("div.section", func(e *colly.HTMLElement) {
-		fmt.Println("something new found")
-		fmt.Println(e.ChildText("p"))
-	})
-
-	// Before making a request print "Visiting ..."
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-
 	c.Limit(&colly.LimitRule{
 		 DomainGlob:  "*",
-		 RandomDelay: 1 * time.Second,
+		 RandomDelay: 2 * time.Second,
  })
 
 	// Start scraping on seed
