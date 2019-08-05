6b689e6ca6548df672a01dc8fb00f9a445ed3c06
Aaruran Elamurugaiyan <aarurane@gmail.com>
2019-08-04 21:32:03 -0400

    wrote a simple go script to encode the python docs as integers

---
+++ encode_text.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"encoding/csv"
+	"bufio"
+	"io"
+	"io/ioutil"
+	"sync"
+	// "strconv"
+	"flag"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readMapToCsv(filename string) map[string]string{
+	m := make(map[string]string)
+	in, err := os.Open(filename)
+	check(err)
+	r := csv.NewReader(in)
+	for {
+			record, err := r.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				panic(err)
+			}
+			m[record[0]] = record[1]
+	}
+	fmt.Println(m)
+	return m
+}
+
+func encodeText(sourceFilename, targetFilename string, m *map[string]string, wg *sync.WaitGroup) {
+	defer (*wg).Done()
+	in, err := os.Open(sourceFilename)
+	check(err)
+
+	s := bufio.NewScanner(in)
+	s.Split(bufio.ScanRunes)
+
+	t, err := os.Create(targetFilename)
+	check(err)
+	defer t.Close()
+	// result := make(map[string] int)
+	fmt.Println("encoding:", sourceFilename)
+	for s.Scan() {
+		c := s.Text()
+		newC, exists := (*m)[c]
+		if ! exists {
+			newC = (*m)[" "]
+		}
+		t.WriteString(newC + " ")
+	}
+	fmt.Println("finished with:", sourceFilename)
+	return
+}
+
+
+func main() {
+	rootDirectoryPtr := flag.String("dir", "example_text_files/", "The directory holding all .txt files to count")
+	targetDirectoryPtr := flag.String("target", "cleaned_examples/", "The directoy which will hold the encoded .txt files")
+	flag.Parse()
+	rootDirectory := *rootDirectoryPtr
+	targetDirectory := *targetDirectoryPtr
+	c, err := ioutil.ReadDir(rootDirectory)
+	check(err)
+
+	var wg sync.WaitGroup
+	numberOfFiles := len(c)
+	wg.Add(numberOfFiles)
+	// Use a buffered channel, as it is non-blocking
+	converter := readMapToCsv("char_int_map.csv")
+
+	for _, v := range c {
+		sourceFilename := rootDirectory + v.Name()
+		targetFilename := targetDirectory + v.Name()
+		fmt.Println("sourceFilename:", sourceFilename)
+		fmt.Println("targetFilename:", targetFilename)
+		go encodeText(sourceFilename,targetFilename, &converter, &wg)
+	}
+	wg.Wait()
+}
