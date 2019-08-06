1f7cb889bfa13cbd2d375848ce0478d2e63a0a9e
Aaruran Elamurugaiyan <aarurane@gmail.com>
2019-08-06 17:21:59 -0400

    finished refactoring and naming conventions, and a bug in my data processing

---
+++ src/features/txt-to-numpy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# source/inspiration: https://github.com/keras-team/keras/blob/master/examples/lstm_text_generation.py
+import numpy as np
+import os
+import pandas as pd
+from tqdm import tqdm
+
+maxlen = 40 # tunable parameter, but fine for now
+number_of_characters = 104
+phrases = []
+next_chars = []
+source_directory = '../data/interim/cleaned_corpus'
+print('Converting interim data to numpy arrays...')
+for f in tqdm(os.listdir(source_directory)):
+    file_name = source_directory + '/' + f
+    with open(file_name, 'r') as src:
+        body = src.read()
+        str_tokens = body.split()
+        tokens = [int(i) for i in str_tokens]
+        for i in range(len(tokens) - maxlen):
+            phrases.append(tokens[i:i + maxlen])
+            next_chars.append(tokens[i+maxlen])
+
+x_data = np.array(phrases)
+y_data = np.array(next_chars)
+print('Saving to npz file')
+target_file = '../data/processed/compressed_data.npz'
+np.savez_compressed(target_files, x_data=x_data, y_data=y_data)
+print('Done.')
